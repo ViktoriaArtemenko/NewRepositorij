e98fbcdbb1912a50daf886b9a405cdb70348e946
java-1-02 <videothebestmusically@gmail.com>
2017-02-22 16:00:24 +0200

    new code

--- src/com/my/librery/Main.java
+++ src/com/my/librery/Main.java
@@ -1,8 +1,112 @@
 package com.my.librery;
 
+import java.util.Random;
+
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    private static int[] mas;
+    private static int n;
+    private static Random random = new Random();
+    private static int n1, n2, n3, n4;
+
+    public static void main(String[] args) throws InterruptedException {
+
+        n = 10;//random.nextInt(100) + 4;
+        mas = new int[n];
+
+        for (int i = 0; i < mas.length; i++) {
+            mas[i] = random.nextInt(100);
+        }
+
+        n1 = n2 = n3 = mas.length / 4;
+        n4 = mas.length - (n1 * 3);
+
+        printMas(mas);
+
+        System.out.println();
+        System.out.println(n1 + "  " + n2 + "  " + n3 + "  " + n4);
+
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < n1; i++) {
+                    System.out.print("t1 " + mas[i] + " ");
+                }
+            }
+        });
+
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = n1; i < n2 * 2; i++) {
+                    System.out.print("t2 " + mas[i] + " ");
+                }
+            }
+        });
+
+
+        Thread t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = n2 * 2; i < n3 * 3; i++) {
+                    System.out.print("t3 " + mas[i] + " ");
+                }
+            }
+        });
+
+        Thread t4 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = n3 * 3; i < mas.length; i++) {
+                    System.out.print("t4 " + mas[i] + " ");
+                }
+            }
+        });
+
+        Thread t5 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        });
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+        t5.start();
+        t5.wait();
+        t5.wait();
+        t5.wait();
+        t5.wait();
+
+    }
+
+    public static void printMas(int[] array) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static int getMaxInt(int start, int end) {
+        int max = 0;
+        for (int i = start; i < end; i++) {
+            if (mas[i] > max) max = mas[i];
+
+        }
+        return max;
+    }
+
+    public static void myThread(Thread t){
+        t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                getMaxInt(n3 * 3,  mas.length);
+            }
+        });
     }
 }
+
---
+++ src/com/my/librery/OldDay.java
@@ -0,0 +1,54 @@
+package com.my.librery;
+
+public class OldDay {
+
+
+    public OldDay(){
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    doSmt("T1");
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 10; i++) {
+                    synchronized (locker) {
+                        System.out.println("T2 notify");
+                        locker.notify();
+                    }
+                    try {
+                        Thread.sleep(10000);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        });
+
+        t1.setName("T1Name");
+        t2.setName("T2Name");
+
+        t1.setPriority(1);
+        t2.setPriority(10);
+
+        t1.start();
+        t2.start();
+    }
+
+    Object locker = new Object();
+
+    public void doSmt(String info) {
+        synchronized (locker) {
+            System.out.println("doSmt begin " + info + " " + Thread.currentThread().getName());
+            try {
+                locker.wait();
+            } catch (InterruptedException e) {
+            }
+            System.out.println("doSmt end " + info + " " + Thread.currentThread().getName());
+        }
+    }
+}
